5ba358abd7863e94fab4d283f06cdb20a1fbc068
DevHur <149660652+Kenlorence@users.noreply.github.com>
2025-09-04 22:13:02 +0800

    learned data conversion

--- Calculator/try.js
+++ Calculator/try.js
@@ -1,4 +1,4 @@
-// user input in java scrpt
+/*user input in java scrpt
 let user;
 
 document.getElementById("Submit").onclick = function() {
@@ -9,4 +9,25 @@ user = document.getElementById("username").value;
 alert("Welcome " + user + " to my website");
 document.getElementById("myH1").textContent = "Welcome " + user + " to my website";
 }
+*/
+// type conversion 
+/*
+let age = window.prompt("What is your age?");
+age = Number(age);
+// age = parseInt(age);
+// age = parseFloat(age);
+age += 10;
 
+console.log( age, typeof age);
+
+let x ;
+let y ;
+let z ;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+ */
