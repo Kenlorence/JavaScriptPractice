90ea162fe453f167576ac0616ab47911626d01c6
DevHur <149660652+Kenlorence@users.noreply.github.com>
2025-09-05 14:07:30 +0800

    Math method

---
+++ Math/math.js
@@ -0,0 +1,19 @@
+let x = 10.21;
+let y = 2;
+let z = -2.34;
+/* Math object methods
+z = Math.round(x);
+z = Math.floor(x);
+ 
+z = Math.ceil(x); */
+//z = Math.trunc(x);
+//z = Math.pow(x, y); 
+
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.abs(z);
+//z = Math.sign(z);
+//let max = Math.max(10, 20, 30, 40, 50, -100, 200);
+//let min = Math.min(-1000, 20, 30, 40, 50, -100, 200);
+//let min = Math.min(3, 5, 1, 7, -2, 8);
+console.log(min); // 3
